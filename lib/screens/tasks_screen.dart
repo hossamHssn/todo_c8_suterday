7f45dc439d2ed6c5ad1268ead7909e522d87e86a
hossam <hossamhassnh22@gmail.com>
2023-05-13 22:34:02 +0300

    finish task tab ui

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,12 +1,47 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:to_do_c8_sutrday/screens/widgets/task%20widget.dart';
 
-class tasks_screen extends StatelessWidget {
-  const tasks_screen({Key? key}) : super(key: key);
+class tasks_screen extends StatefulWidget {
+  DateTime date = DateTime.now();
 
   @override
+  State<tasks_screen> createState() => _tasks_screenState();
+}
+
+class _tasks_screenState extends State<tasks_screen> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Column(
+      children: [
+        DatePicker(
+          DateTime.now(),
+          initialSelectedDate: DateTime.now(),
+          selectionColor: Theme.of(context).primaryColor,
+          selectedTextColor: Colors.white,
+          height: 100,
+          onDateChange: (newDate) {
+            // New date selected
+            setState(() {
+              newDate = newDate;
+            });
+          },
+        ),
+        SizedBox(
+          height: 18,
+        ),
+        Expanded(
+          child: ListView.separated(
+            separatorBuilder: (context, index) => SizedBox(
+              height: 10,
+            ),
+            itemBuilder: (context, index) {
+              return taskWidget();
+            },
+            itemCount: 5,
+          ),
+        )
+      ],
     );
   }
 }
